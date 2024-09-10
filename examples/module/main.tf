b8edfd7a0390053c54cffa527f03e61e31cc5785
dnogu <44304108+dnogu@users.noreply.github.com>
2024-09-10 16:27:45 -0400

    Update main.tf

--- examples/module/main.tf
+++ examples/module/main.tf
@@ -3,5 +3,5 @@ module "github_repo_factory" {
     # source = "https://github.com/dnogu/terraform-github-repository.git@latest"
 
     repository_name = var.repository_name
-    issue_labels_labels = var.issue_labels_labels
+    issue_label_labels = var.issue_label_labels
 }
