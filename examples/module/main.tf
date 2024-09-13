b35212c1100850401d448734f800a613c887b697
DN <DN@DN.local>
2024-09-12 23:37:14 -0400

    add collaborators resourcd, convert issue label to single resource with dynamic blocks

--- examples/module/main.tf
+++ examples/module/main.tf
@@ -1,7 +1,8 @@
 module "github_repo_factory" {
+    # source = "dnogu/repository/github"
     source = "../../"
-    # source = "https://github.com/dnogu/terraform-github-repository.git@latest"
 
     repository_name = var.repository_name
     issue_label_labels = var.issue_label_labels
+    github_repository_collaborators_collaborators = var.github_repository_collaborators_collaborators
 }
