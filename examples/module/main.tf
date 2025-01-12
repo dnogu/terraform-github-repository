c1b339de3e5258e495b1332ad65b7ab2f5970851
DN <DN@DN.local>
2025-01-12 17:33:54 -0500

    fix and test output

--- examples/module/main.tf
+++ examples/module/main.tf
@@ -5,4 +5,5 @@ module "github_repo_factory" {
   repository_name                               = var.repository_name
   issue_label_labels                            = var.issue_label_labels
   github_repository_collaborators_collaborators = var.github_repository_collaborators_collaborators
+  github_repository_file_files_and_content      = var.github_repository_file_files_and_content
 }
--- examples/module/vars.tf
+++ examples/module/vars.tf
@@ -15,3 +15,17 @@ variable "github_repository_collaborators_collaborators" {
   type    = map(string)
   default = {}
 }
+
+variable "github_repository_file_files_and_content" {
+  type = list(object({
+    file          = string
+    content       = optional(string)
+    file_location = optional(string)
+  }))
+  description = "Files to create"
+  default     = []
+  validation {
+    condition     = alltrue([for f in var.github_repository_file_files_and_content : f.content != null || f.file_location != null])
+    error_message = "Each file must have either 'content' or 'file_location' set."
+  }
+}
